89ea7e9ec01eabf4d89d7b3fbe5fafaa34001b6d
soffiayanchashvili <119891979+soffiayanchashvili@users.noreply.github.com>
2023-07-03 19:15:30 +0400

    idk what i did

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [data, setData] = useState([
+    { id: 0, name: "სალომე", faculty: null },
+    { id: 1, name: "ანი", faculty: null },
+    { id: 2, name: "მარიამი", faculty: null },
+    { id: 3, name: "ნინი", faculty: null },
+  ]);
+
+
+  const randomize = () => {
+    data.forEach((student, index) => {
+      setTimeout(() => {
+        setData((prev) =>
+          prev.map((item) =>
+            item.id === student.id
+              ? { ...item, faculty: Math.floor(Math.random() * 4) }
+              : item
+          )
+        );
+      }, index * 1000);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul class="list-group container text-start" style={{ width: "300px" }}>
+          {data?.map(
+            (student) =>
+              student.faculty === null && (
+                <li key={student?.id} class="list-group-item">
+                  {student?.name}
+                </li>
+              )
+          )}
+        </ul>
+      <button onClick={randomize}  class="btn btn-primary mt-5">გამანაწილებელი ქუდი</button>
+      <div className="d-flex align-items-start gap-2 mt-5">
+          <div>
+            <h4>გრიფინდორი</h4>
+            <ul class="list-group container" style={{ width: "300px" }}>
+              {data?.map(
+                (student) =>
+                  student.faculty === 0 && (
+                    <li key={student?.id} class="list-group-item">
+                      {student?.name}
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+          <div>
+            <h4>რეივენქლო</h4>
+            <ul class="list-group container" style={{ width: "300px" }}>
+              {data?.map(
+                (student) =>
+                  student.faculty === 1 && (
+                    <li key={student?.id} class="list-group-item">
+                      {student?.name}
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+          <div>
+            <h4>ჰაფლფაპი</h4>
+            <ul class="list-group container" style={{ width: "300px" }}>
+              {data?.map(
+                (student) =>
+                  student.faculty === 2 && (
+                    <li key={student?.id} class="list-group-item">
+                      {student?.name}
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+          <div>
+            <h4>სლიზერინი</h4>
+            <ul class="list-group container" style={{ width: "300px" }}>
+              {data?.map(
+                (student) =>
+                  student.faculty === 3 && (
+                    <li key={student?.id} class="list-group-item">
+                      {student?.name}
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+        </div>
     </div>
   );
 }
